cfca8ecfebb36bbdbe8b1f622dce79212e26e143
Gareth Evans <gareth@bryncynfelin.co.uk>
2017-07-06 09:40:29 +0000

    Added stackdriver nozzle to cf deployment

--- create-cf.sh
+++ create-cf.sh
@@ -6,24 +6,36 @@ export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=`bosh int ../creds.yml --path /admin_password`
 MODEL=base
 
+export service_account_email=stackdriver-nozzle@finkit-cf-gcp-exp-01.iam.gserviceaccount.com
+
+if [ ! -f ${service_account_email}.key.json ]; then
+    gcloud iam service-accounts keys create ${service_account_email}.key.json \
+            --iam-account ${service_account_email}
+fi
+
 bosh -e 10.0.0.6 update-cloud-config cloud-config.yml
 
 bosh -e 10.0.0.6 upload-stemcell https://bosh.io/d/stemcells/bosh-google-kvm-ubuntu-trusty-go_agent?v=3421.6
+bosh -e 10.0.0.6 upload-release https://storage.googleapis.com/bosh-gcp/beta/stackdriver-tools/latest.tgz
 
 bosh int --vars-store ../cf-deployment-vars.yml \
+    --var-file gcp_credentials_json=${service_account_email}.key.json \
     -o ../cf-deployment/operations/gcp.yml \
     -o downsize.yml \
     -o downsize-diego-cell-disk-usage.yml \
     -o log-trace-tokens.yml \
+    -o add-stackdriver-nozzle.yml \
     -o ${MODEL}-model.yml \
     --var-errs \
     ../cf-deployment/cf-deployment.yml
 
 bosh -e 10.0.0.6 -d cf deploy \
+    --var-file gcp_credentials_json=${service_account_email}.key.json \
     --vars-store ../cf-deployment-vars.yml \
     -o ../cf-deployment/operations/gcp.yml \
     -o downsize.yml \
     -o downsize-diego-cell-disk-usage.yml \
     -o log-trace-tokens.yml \
+    -o add-stackdriver-nozzle.yml \
     -o ${MODEL}-model.yml \
     ../cf-deployment/cf-deployment.yml
