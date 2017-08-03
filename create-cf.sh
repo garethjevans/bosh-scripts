9ae67c62d9eb7bb72a60bd20414ff2745161cb67
Gareth Evans <gareth@bryncynfelin.co.uk>
2017-08-03 09:08:16 +0000

    Performing bosh deployments as non-interactive

--- create-cf.sh
+++ create-cf.sh
@@ -4,6 +4,8 @@ bosh alias-env gcpbosh -e 10.0.0.6 --ca-cert <(bosh int ../creds.yml --path /dir
 
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=`bosh int ../creds.yml --path /admin_password`
+export BOSH_NON_INTERACTIVE=true
+
 MODEL=base
 CF_DEPLOYMENT_VERSION=v0.16.0
 
