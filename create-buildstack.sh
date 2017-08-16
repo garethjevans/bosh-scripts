7a70a652445e8124dc06035ec06dc8371f725081
Gareth Evans <gareth@bryncynfelin.co.uk>
2017-08-16 08:51:11 +0000

    Using vars for generated passwords when deploying the buildstack

--- create-buildstack.sh
+++ create-buildstack.sh
@@ -4,15 +4,15 @@ bosh alias-env gcpbosh -e 10.0.0.6 --ca-cert <(bosh int ../creds.yml --path /dir
 
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=`bosh int ../creds.yml --path /admin_password`
+export BOSH_ENVIRONMENT=gcpbosh
 
-bosh -e 10.0.0.6 update-cloud-config cloud-config.yml
+bosh update-cloud-config cloud-config.yml --non-interactive
 
-bosh -e 10.0.0.6 upload-release https://storage.googleapis.com/buildstack-blobs/buildstack-boshrelease.tgz
+cd ../buildstack-boshrelease
+git pull
+bosh create-release --force
+bosh upload-release
 
-bosh int --vars-store ../buildstack-deployment-vars.yml \
-    --var-errs \
-    buildstack-deployment.yml
-
-bosh -e 10.0.0.6 -d buildstack deploy \
+bosh -d buildstack deploy \
     --vars-store ../buildstack-deployment-vars.yml \
-    buildstack-deployment.yml
+    buildstack.yml
