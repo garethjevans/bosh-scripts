d683a4b465f50da6b046612c0ba1d0ad555ee9d8
Gareth Evans <gareth@bryncynfelin.co.uk>
2017-08-23 10:33:36 +0000

    Updated cf-deployment to v0.21.0

--- create-cf.sh
+++ create-cf.sh
@@ -6,7 +6,7 @@ export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=`bosh int ../creds.yml --path /admin_password`
 
 MODEL=base
-CF_DEPLOYMENT_VERSION=v0.19.0
+CF_DEPLOYMENT_VERSION=v0.21.0
 
 cd ../cf-deployment
 git checkout master
@@ -23,20 +23,22 @@ fi
 
 bosh -e 10.0.0.6 update-cloud-config cloud-config.yml --non-interactive
 bosh -e 10.0.0.6 update-runtime-config runtime-config.yml --non-interactive
-
+#bosh -e 10.0.0.6 upload-stemcell https://bosh.io/d/stemcells/bosh-google-kvm-ubuntu-trusty-go_agent?v=3445.2 --non-interactive
 bosh -e 10.0.0.6 upload-stemcell https://bosh.io/d/stemcells/bosh-google-kvm-ubuntu-trusty-go_agent?v=3421.11 --non-interactive
 bosh -e 10.0.0.6 upload-release https://storage.googleapis.com/bosh-gcp/beta/stackdriver-tools/latest.tgz --non-interactive
 
-#bosh int --vars-store ../cf-deployment-vars.yml \
-#    --var-file gcp_credentials_json=${service_account_email}.key.json \
-#    -o ../cf-deployment/operations/gcp.yml \
-#    -o rolling-updates-to-diego-cells.yml \
-#    -o downsize.yml \
-#    -o log-trace-tokens.yml \
-#    -o add-stackdriver-nozzle.yml \
-#    -o ${MODEL}-model.yml \
-#    --var-errs \
-#    ../cf-deployment/cf-deployment.yml
+bosh int --vars-store ../cf-deployment-vars.yml \
+    --var-file gcp_credentials_json=${service_account_email}.key.json \
+    -o ../cf-deployment/operations/gcp.yml \
+    -o rolling-updates-to-diego-cells.yml \
+    -o downsize.yml \
+    -o log-trace-tokens.yml \
+    -o add-stackdriver-nozzle.yml \
+    -o ${MODEL}-model.yml \
+    --var-errs \
+    --var-errs-unused \
+    ../cf-deployment/cf-deployment.yml
+
 
 bosh -e 10.0.0.6 -d cf deploy \
     --var-file gcp_credentials_json=${service_account_email}.key.json \
@@ -48,3 +50,4 @@ bosh -e 10.0.0.6 -d cf deploy \
     -o add-stackdriver-nozzle.yml \
     -o ${MODEL}-model.yml \
     ../cf-deployment/cf-deployment.yml
+
