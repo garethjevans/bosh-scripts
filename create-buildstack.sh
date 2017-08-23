620e5c38ab8eefef530c9718324603ad0bf6c327
Gareth Evans <gareth@bryncynfelin.co.uk>
2017-08-23 10:34:58 +0000

    Updated buildstack creation script

--- create-buildstack.sh
+++ create-buildstack.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-bosh alias-env gcpbosh -e 10.0.0.6 --ca-cert <(bosh int ../creds.yml --path /director_ssl/ca)
+#bosh alias-env gcpbosh -e 10.0.0.6 --ca-cert <(bosh int ../creds.yml --path /director_ssl/ca)
 
-export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=`bosh int ../creds.yml --path /admin_password`
-export BOSH_ENVIRONMENT=gcpbosh
+#export BOSH_CLIENT=admin
+#export BOSH_CLIENT_SECRET=`bosh int ../creds.yml --path /admin_password`
+#export BOSH_ENVIRONMENT=gcpbosh
 
 bosh update-cloud-config cloud-config.yml --non-interactive
 
@@ -13,6 +13,14 @@ git pull
 bosh create-release --force
 bosh upload-release
 
+echo "========================================================="
+bosh -d buildstack int \
+    --vars-store ../buildstack-deployment-vars.yml \
+    $* \
+    buildstack.yml
+echo "========================================================="
+
 bosh -d buildstack deploy \
     --vars-store ../buildstack-deployment-vars.yml \
-    buildstack.yml
+    $* \
+    buildstack.yml 
